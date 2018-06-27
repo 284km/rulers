45cd7dcd4f259b620ca455c31c673778aed96bf9
284km <at284km@gmail.com>
2018-06-27 15:59:59 +0900

    Controller

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -1,15 +1,31 @@
 require "rulers/version"
 require 'rulers/array'
+require 'rulers/routing'
 
 module Rulers
   class Application
     def call(env)
+      klass, act = get_controller_and_action(env)
+      controller = klass.new(env)
+      text = controller.send(act)
+
       system("echo debug > debug.txt")
       [
         200,
         {'Content-Type' => 'text/html'},
-        ["Hello from Ruby on Rulers! #{[1,2,3].hello}"]
+        # ["Hello from Ruby on Rulers! #{[1,2,3].hello}"]
+        [text]
       ]
+    end
+  end
+
+  class Controller
+    def initialize(env)
+      @env = env
+    end
+
+    def env
+      @env
     end
   end
 end
