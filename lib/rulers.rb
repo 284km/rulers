7fcbe879d8a0b449eb34df92c2175e58833bc071
284km <at284km@gmail.com>
2018-06-27 12:06:43 +0900

    Exercise One: Reloading Rulers

--- lib/rulers.rb
+++ lib/rulers.rb
@@ -3,6 +3,7 @@ require "rulers/version"
 module Rulers
   class Application
     def call(env)
+      system("echo debug > debug.txt")
       [
         200,
         {'Content-Type' => 'text/html'},
