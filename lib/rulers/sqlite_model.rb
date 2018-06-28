25134158517dd34f544a308f1ae2fd1064b0ed5c
284km <at284km@gmail.com>
2018-06-29 03:50:06 +0900

    Implement Model::SQLite.find

--- lib/rulers/sqlite_model.rb
+++ lib/rulers/sqlite_model.rb
@@ -10,6 +10,23 @@ module Rulers
         @hash = data
       end
 
+      def self.find(id)
+        row = DB.execute <<SQL
+select #{schema.keys.join ","} from #{table}
+where id = #{id};
+SQL
+        data = Hash[schema.keys.zip row[0]]
+        self.new data
+      end
+
+      def [](name)
+        @hash[name.to_s]
+      end
+
+      def []=(name, value)
+        @hash[name.to_s] = value
+      end
+
       def self.to_sql(val)
         case val
         when NilClass
